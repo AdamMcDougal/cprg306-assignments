9dbe60ead69ffdae8eb662acd2192737e907b8bc
AMcdougal <31479662+AdamMcDougal@users.noreply.github.com>
2024-01-23 14:42:30 -0700

    Add week 2 lab

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Adam McDougal</h1>
+      <Link href="https://github.com/AdamMcDougal/cprg306-assignments">Github</Link>
+    </main>
+  );
+}
