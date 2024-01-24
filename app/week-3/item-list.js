f08811d56fdb0f3a006a058a2fb5bb00fff601c2
AMcdougal <31479662+AdamMcDougal@users.noreply.github.com>
2024-01-23 17:57:57 -0700

    Add lab 3

---
+++ app/week-3/item-list.js
@@ -0,0 +1,92 @@
+import Item from "./item";
+
+export default function ItemList() {
+        const item1 = {
+            name: "milk, 4 L 🥛",
+            quantity: 1,
+            category: "dairy",
+          };
+           
+          const item2 = {
+            name: "bread 🍞",
+            quantity: 2,
+            category: "bakery",
+          };
+           
+          const item3 = {
+            name: "eggs, dozen 🥚",
+            quantity: 2,
+            category: "dairy",
+          };
+           
+          const item4 = {
+            name: "bananas 🍌",
+            quantity: 6,
+            category: "produce",
+          };
+           
+          const item5 = {
+            name: "broccoli 🥦",
+            quantity: 3,
+            category: "produce",
+          };
+           
+          const item6 = {
+            name: "chicken breasts, 1 kg 🍗",
+            quantity: 1,
+            category: "meat",
+          };
+           
+          const item7 = {
+            name: "pasta sauce 🍝",
+            quantity: 3,
+            category: "canned goods",
+          };
+           
+          const item8 = {
+            name: "spaghetti, 454 g 🍝",
+            quantity: 2,
+            category: "dry goods",
+          };
+           
+          const item9 = {
+            name: "toilet paper, 12 pack 🧻",
+            quantity: 1,
+            category: "household",
+          };
+           
+          const item10 = {
+            name: "paper towels, 6 pack",
+            quantity: 1,
+            category: "household",
+          };
+           
+          const item11 = {
+            name: "dish soap 🍽️",
+            quantity: 1,
+            category: "household",
+          };
+           
+          const item12 = {
+            name: "hand soap 🧼",
+            quantity: 4,
+            category: "household",
+          };
+
+          return (
+            <main>
+                <Item {...item1} />
+                <Item {...item2} />
+                <Item {...item3} />
+                <Item {...item4} />
+                <Item {...item5} />
+                <Item {...item6} />
+                <Item {...item7} />
+                <Item {...item8} />
+                <Item {...item9} />
+                <Item {...item10} />
+                <Item {...item11} />
+                <Item {...item12} />
+            </main>
+          )
+}
